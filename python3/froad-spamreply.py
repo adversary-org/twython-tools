464cb8ea74c1285a0e2d6da73303da8d00f0fa72
Ben McGinnes <ben@adversary.org>
2014-05-20 09:28:19 +1000

    Remove hashtag option and username check (must use foad.py option with username).

--- python3/froad-spamreply.py
+++ python3/froad-spamreply.py
@@ -43,25 +43,14 @@ if l == 2:
     wtf = sys.argv[1]
     victim = input("* Recipient: ")
     stat = input("* Twitter ID for status being replied to: ")
-    tags = input("Append hashtags (optional, enter as they appear): ")
 elif l == 3:
     wtf = sys.argv[1]
     victim = sys.argv[2]
     stat = input("* Twitter ID for status being replied to: ")
-    tags = input("Append hashtags (optional, enter as they appear): ")
-elif l == 4:
+elif l >= 4:
     wtf = sys.argv[1]
     victim = sys.argv[2]
     stat = sys.argv[3]
-    tags = input("Append hashtags (optional, enter as they appear): ")
-elif l >= 5:
-    wtf = sys.argv[1]
-    victim = sys.argv[2]
-    stat = sys.argv[3]
-    tag = []
-    for i in range(l - 4):
-        tag.append(str(sys.argv[i + 4]))
-    tags = " ".join(tag)
 elif l < 2:
     wtf = input("* Type of fuck to give: ")
     victim = input("* Recipient: ")
@@ -74,21 +63,9 @@ else:
     tags = input("Append hashtags (optional, enter as they appear): ")
 
 target = "@" + victim
-msg = p(foad +" "+ wtf +" "+ target, shell=True).strip()
-
-if target in msg:
-    message = msg
-elif target not in msg:
-    message = target + ", " + msg[0:4].lower() + msg[4:len(msg)]
-
-if len(message) + len(tags) <= 135:
-    mesg = message +" "+tags
-elif len(message) + len(tag[0]) <= 135:
-    mesg = message +" "+tag[0]
-else:
-    mesg = message
+mesg = p(foad +" "+ wtf +" "+ target, shell=True).strip()
 
 print(mesg.decode("utf-8", "strict"))
 
-twitter.update_status(status=message, in_reply_to_status_id=stat)
+twitter.update_status(status=mesg, in_reply_to_status_id=stat)
 twitter.report_spam(screen_name=victim)
