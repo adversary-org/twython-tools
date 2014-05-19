243ea7b657c947033ab63a76828c52fe512f8f9a
Ben McGinnes <ben@adversary.org>
2014-05-19 21:30:41 +1000

    Header updates.

--- python2/view-credentials.py
+++ python2/view-credentials.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 
-# Copyright (C) Ben McGinnes, 2013
+##
+# Copyright (C) Ben McGinnes, 2013-2014
 # ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
 #
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+# Requirements:
+#
+# See Documentation/README-py2.txt
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__license__ = "BSD"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
 
 import pprint
 from twython import Twython
--- python2/view-friends.py
+++ python2/view-friends.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 
-# Copyright (C) Ben McGinnes, 2013
+##
+# Copyright (C) Ben McGinnes, 2013-2014
 # ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
 #
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+# Requirements:
+#
+# See Documentation/README-py2.txt
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__license__ = "BSD"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
 
 import math
 from twython import Twython
 from config import *
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-#creds = twitter.verify_credentials()
 
 user = raw_input("Enter Twitter handle to get friends of: ")
 suser = twitter.show_user(screen_name=user)
