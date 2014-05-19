3bffbe47b6c80301d823a5472b629400a6170c6a
Ben McGinnes <ben@adversary.org>
2014-05-20 04:37:22 +1000

    Added script for favouriting a tweet.

---
+++ python3/tweet-favourite.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+##
+# Copyright (C) Ben McGinnes, 2013-2014
+# ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
+#
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+# Requirements:
+#
+# * Python 3.2 or later (developed with Python 3.4.x)
+#
+# Options and notes:
+#
+# Usage:  
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__license__ = "BSD"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
+import sys
+from twython import Twython, TwythonError
+from config import *
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+l = len(sys.argv)
+
+if l == 1:
+    twid = input("Enter ID number of tweet to favourite: ")
+elif l >= 2:
+    twid = sys.argv[1]
+
+try:
+    twitter.create_favorite(id=twid)
+except TwythonError as e:
+    print(e)
