48a18ffe70007a6d362d5eee7f4f6fa43ff9484d
Ben McGinnes <ben@adversary.org>
2014-05-20 17:07:41 +1000

    Alternate method of displaying a tweet from the full URL.

---
+++ current/tweet-urlshow.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+##
+# Copyright (C) Ben McGinnes, 2013-2014
+# ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
+#
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+# Requirements:
+#
+# * Python 3.2 or later (developed with Python 3.4.x)
+#
+# Options and notes:
+#
+# Usage:  
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__license__ = "BSD"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
+import sys
+from twython import Twython, TwythonError
+from config import *
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+l = len(sys.argv)
+
+if l == 1:
+    url = input("Enter full URL of tweet: ")
+elif l >= 2:
+    url = sys.argv[1]
+
+turl = url.replace(url[0:20], "").replace("/status/", " ").split()
+twid = int(turl[1])
+
+try:
+    tweet = twitter.show_status(id=twid)
+    name = tweet["user"]["name"]
+    user = tweet["user"]["screen_name"]
+    text = tweet["text"]
+    show = name+" ("+user+"): "+text
+    print(show)
+except TwythonError as e:
+    print(e)
