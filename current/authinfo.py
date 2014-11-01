db664333a0efef6a9522778dc2b787cf24f2ffcf
Ben McGinnes <ben@adversary.org>
2014-11-01 15:52:16 +1100

    Moved the Tor connection details out of authinfo.py and into a new file.  This will make it easier for future settings enabling a permanent configuration file.  It is likely that some users will want that permanently set or to set it via a command line flag.

--- current/authinfo.py
+++ current/authinfo.py
@@ -6,7 +6,7 @@
 # ben@adversary.org
 # OpenPGP/GPG key:  0x321E4E2373590E5D
 #
-# Version:  0.0.6
+# Version:  0.0.7
 #
 # BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
 # License:  BSD
@@ -15,9 +15,7 @@
 #
 # * Python 3.2 or later (developed with Python 3.4.x).
 # * Converted from scripts initially developed with Python 2.7.x.
-# * python-gnupg 0.3.6 or later.
-# * GNU Privacy Guard (GnuPG, GPG) 1.4.x or 2.0.x).
-# * Tor service with SOCKS and proxy (optional).
+# * PyCrypto 2.6.1 or later.
 #
 # Options and notes:
 #
@@ -34,7 +32,7 @@ __copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2014"
 __copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
 __copyrightu__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
 __license__ = "BSD"
-__version__ = "0.0.6"
+__version__ = "0.0.7"
 __bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
 
 
@@ -59,7 +57,6 @@ except getpass.GetPassWarning:
 
 phrase = hashlib.sha256(password.encode("utf-8")).hexdigest()
 del password
-torcon = input("Will you be using Tor to access Twitter (Y/N): ")
 
 files = ["oauth1.txt.asc", "oauth2.txt.asc", "oauth3.txt.asc", "oauth4.txt.asc"]
 authdata = []
@@ -80,49 +77,3 @@ OAUTH_TOKEN_SECRET = authdata[3]
 
 del phrase
 
-affirmative = ["Y", "yes", "y", "1", "true", "aye", 1, True]
-negative = ["N", "no", "n", "0", "false", "nay", 0, False]
-
-if torcon.lower() in negative:
-    client_args = {
-        "verify": True,
-        "headers": {
-            "User-Agent": "Twython"
-            }
-        }
-    print("No Tor configuration set, direct connection enabled.")
-elif torcon.lower() in affirmative:
-    tortype = input("Will you be using the Tor Browser Bundle: ")
-    bundle = ["Y", "yes", "y", "1", "true", "aye", "bundle", 1, True]
-    if tortype.lower() in bundle:
-        client_args {
-            "verify": True,
-            "headers": {
-                "User-Agent": "Twython Over Tor"
-                },
-            "proxies": {
-                "http": "http://127.0.0.1:9150",
-                "https": "https://127.0.0.1:9150",
-                }
-            }
-        print("Tor configuration set on localhost and port 9150")
-    else:
-        client_args = {
-            "verify": True,
-            "headers": {
-                "User-Agent": "Twython Over Tor"
-                },
-            "proxies": {
-                "http": "http://127.0.0.1:9050",
-                "https": "https://127.0.0.1:9050",
-                }
-            }
-        print("Tor configuration set on localhost and port 9050")
-else:
-    client_args = {
-        "verify": True,
-        "headers": {
-            "User-Agent": "Twython"
-            }
-        }
-    print("No Tor configuration set, direct connection enabled.")
