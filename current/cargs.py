213aa7ede552c3dfdae716be568b9609105f751a
Ben McGinnes <ben@adversary.org>
2014-11-02 22:56:33 +1100

    Optional configuration to reduce interactive questions.

--- current/cargs.py
+++ current/cargs.py
@@ -6,24 +6,24 @@
 # ben@adversary.org
 # OpenPGP/GPG key:  0x321E4E2373590E5D
 #
-# Version:  0.0.8
+# Version:  0.0.9
 #
 # BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
 # License:  BSD
 #
-# Requirements:
-#
-# * Python 3.2 or later (developed with Python 3.4.x).
-# * Converted from scripts initially developed with Python 2.7.x.
-# * Tor service with SOCKS support (optional)
-#
 # Options and notes:
 #
-# The config.py file must be customised prior to running either
-# gen-auth.py or authinfo.py in order to set the correct path for the
-# GPG configuration and adjust other settings.
+# It may be preferable to delete or comment out the class not being
+# used.
 #
 # Usage:  
+#
+# This file and the classes in it are imported by config.py.
+# The permanent class removes the need to answer questions about
+# connecting through Tor and simply checks for the connection types,
+# first for a custom installation of Tor, then for the Tor Browser
+# Bundle and if neither are available it attempts a direct (ordinary)
+# connection.
 #
 ##
 
@@ -32,27 +32,42 @@ __copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2014"
 __copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
 __copyrightu__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
 __license__ = "BSD"
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
 
+import socket
 
-torcon = input("Will you be using Tor to access Twitter (Y/N): ")
 
-affirmative = ["Y", "yes", "y", "1", "true", "aye", 1, True]
-negative = ["N", "no", "n", "0", "false", "nay", 0, False]
+class permanent:
+    def checktor1():
+        try:
+            s = socket.socket()
+            s.connect(("127.0.0.1", 9050))
+            return True
+        except socket.error, e:
+            return False
 
-if torcon.lower() in negative:
-    client_args = {
-        "verify": True,
-        "headers": {
-            "User-Agent": "Twython"
+    def checktor2():
+        try:
+            s = socket.socket()
+            s.connect(("127.0.0.1", 9150))
+            return True
+        except socket.error, e:
+            return False
+
+    if checktor1() is True:
+        client_args = {
+            "verify": True,
+            "headers": {
+                "User-Agent": "Twython Over Tor"
+                },
+            "proxies": {
+                "http": "http://127.0.0.1:9050",
+                "https": "https://127.0.0.1:9050",
+                }
             }
-        }
-    print("No Tor configuration set, direct connection enabled.")
-elif torcon.lower() in affirmative:
-    tortype = input("Will you be using the Tor Browser Bundle: ")
-    bundle = ["Y", "yes", "y", "1", "true", "aye", "bundle", 1, True]
-    if tortype.lower() in bundle:
+        print("Tor configuration set on localhost and port 9050")
+    elif checktor2() is True:
         client_args = {
             "verify": True,
             "headers": {
@@ -68,19 +83,57 @@ elif torcon.lower() in affirmative:
         client_args = {
             "verify": True,
             "headers": {
-                "User-Agent": "Twython Over Tor"
-                },
-            "proxies": {
-                "http": "http://127.0.0.1:9050",
-                "https": "https://127.0.0.1:9050",
+                "User-Agent": "Twython"
                 }
             }
-        print("Tor configuration set on localhost and port 9050")
-else:
-    client_args = {
-        "verify": True,
-        "headers": {
-            "User-Agent": "Twython"
+        print("No Tor configuration set, direct connection enabled.")
+
+
+class dynamic:
+    torcon = input("Will you be using Tor to access Twitter (Y/N): ")
+    affirmative = ["Y", "yes", "y", "1", "true", "aye", 1, True]
+    negative = ["N", "no", "n", "0", "false", "nay", 0, False]
+
+    if torcon.lower() in negative:
+        client_args = {
+            "verify": True,
+            "headers": {
+                "User-Agent": "Twython"
+                }
             }
-        }
-    print("No Tor configuration set, direct connection enabled.")
+        print("No Tor configuration set, direct connection enabled.")
+    elif torcon.lower() in affirmative:
+        tortype = input("Will you be using the Tor Browser Bundle: ")
+        bundle = ["Y", "yes", "y", "1", "true", "aye", "bundle", 1, True]
+        if tortype.lower() in bundle:
+            client_args = {
+                "verify": True,
+                "headers": {
+                    "User-Agent": "Twython Over Tor"
+                    },
+                "proxies": {
+                    "http": "http://127.0.0.1:9150",
+                    "https": "https://127.0.0.1:9150",
+                    }
+                }
+            print("Tor configuration set on localhost and port 9150")
+        else:
+            client_args = {
+                "verify": True,
+                "headers": {
+                    "User-Agent": "Twython Over Tor"
+                    },
+                "proxies": {
+                    "http": "http://127.0.0.1:9050",
+                    "https": "https://127.0.0.1:9050",
+                    }
+                }
+            print("Tor configuration set on localhost and port 9050")
+    else:
+        client_args = {
+            "verify": True,
+            "headers": {
+                "User-Agent": "Twython"
+                }
+            }
+        print("No Tor configuration set, direct connection enabled.")
