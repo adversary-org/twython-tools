be11652fb0dba55a2a0152ff4776d3b10b86138f
Ben McGinnes <ben@adversary.org>
2015-03-17 01:17:02 +1100

    Blocked user count
    * Including a count of the number of users blocked.
    * Also changed to str.format from old operator method.

--- ttscripts/view-blocked-users.py
+++ ttscripts/view-blocked-users.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 ##
-# Copyright (C) Ben McGinnes, 2013-2014
+# Copyright (C) Ben McGinnes, 2013-2015
 # ben@adversary.org
 # OpenPGP/GPG key:  0x321E4E2373590E5D
 #
-# Version:  0.0.1
+# Version:  0.0.2
 #
 # BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
 # License:  BSD
@@ -22,8 +22,8 @@
 ##
 
 __author__ = "Ben McGinnes <ben@adversary.org>"
-__copyright__ = "Copyright \u00a9 Benjamin D. McGinnes, 2013-2014"
-__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2014"
+__copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2015"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2015"
 __license__ = "BSD"
 __version__ = "0.0.1"
 __bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
@@ -38,7 +38,8 @@ try:
     blocked = twitter.list_blocks(skip_status=1, cursor=-1)
     busr = blocked["users"]
     bnum = len(busr)
+    print("Number of users blocked: {0}".format(bnum))
     for i in range(bnum):
-        print("%s: %s" % (busr[i]["screen_name"], busr[i]["name"]))
+        print("{0}: {1}".format((busr[i]["screen_name"], busr[i]["name"])))
 except TwythonError as e:
     print(e)
