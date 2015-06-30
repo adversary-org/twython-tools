a5e62445a2bb85fe9d36dbfe3e8ea53aa2db5504
Ben McGinnes <ben@adversary.org>
2015-06-30 20:45:17 +1000

    add-members-mylist
    * Adds one or more users to a list.
    * Theoretically capable of adding more than 100 at a time, but untested.

---
+++ ttscripts/add-members-mylist.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import division
+
+##
+# Copyright (C) Ben McGinnes, 2013-2015
+# ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
+#
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+#
+# Requirements:
+#
+# * Python 3.2 or later (developed with Python 3.4.x)
+#
+# Options and notes:
+#
+# Usage:
+#
+# __name__ <list-slug> user1 user2 user3 etc.
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2015"
+__license__ = "BSD"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
+import math
+import sys
+from twython import Twython, TwythonError
+from config import *
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+"""
+Add one or more usernames to a list of script owner.
+"""
+
+cred = twitter.verify_credentials()
+l = len(sys.argv)
+
+if l == 1:
+    listname = input("Enter the ID or name (slug) of the list: ")
+    targetx = input("Enter usernames or IDs to add (separate by spaces): ")
+elif l == 2:
+    listname = sys.argv[1]
+    targetx = input("Enter usernames or IDs to add (separate by spaces): ")
+elif l >= 3:
+    listname = sys.argv[1]
+    targetx = " ".join(sys.argv[2:l])
+
+try:
+    listid = int(listname)
+except:
+    listid = str(listname)
+
+
+owner = cred["screen_name"]
+targetz = targetx.split()
+targety = []
+lt = len(targetz)
+
+for i in range(lt):
+    if targetz[i].isnumeric is True:
+        target1 = int(targetz[i])
+        target2 = str(targetz[i])
+    else:
+        target1 = str(targetz[i])
+        target2 = None
+
+    if target2 is None:
+        target = target1
+    else:
+        try:
+            t1a = twitter.show_user(user_id=target1)
+            target = t1a["screen_name"]
+        except:
+            target = target2
+
+    targety.append(target)
+
+datachecklist = []
+
+if lt == 1:
+    try:
+        data = twitter.add_list_member(slug=listid, owner_screen_name=owner,
+                                       screen_name=target)
+    except TwythonError as e:
+        print(e)
+        data = None
+    if data is not None:
+        datacheck = True
+    else:
+        datacheck = False
+elif lt >= 2 and lt <= 100:
+    targets = ", ".join(targety)
+    try:
+        data = twitter.create_list_members(slug=listid,
+                                           owner_screen_name=owner,
+                                           user_id=targets)
+    except TwythonError as e:
+        print(e)
+        data = None
+    if data is not None:
+        datacheck = True
+    else:
+        datacheck = False
+elif lt > 100:
+    t0 = lt / 100
+    t1 = math.floor(t0)
+    t2 = t1 * 100
+    t3 = lt - t2
+    for i in range(t1):
+        targets = ", ".join(targety[(i * 100):((i + 1) * 100)])
+        try:
+            data = twitter.create_list_members(slug=listid,
+                                               owner_screen_name=owner,
+                                               user_id=targets)
+        except TwythonError as e:
+            print(e)
+            data = None
+        if data is not None:
+            datachecklist.append(targets)
+        else:
+            pass
+    for i in range(t2, t3):
+        targets = ", ".join(targety[(i * 100):((i + 1) * 100)])
+        try:
+            data = twitter.create_list_members(slug=listid,
+                                               owner_screen_name=owner,
+                                               user_id=targets)
+        except TwythonError as e:
+            print(e)
+            data = None
+        if data is not None:
+            datachecklist.append(targets)
+        else:
+            pass
+    if len(datachecklist) > 0:
+        datacheck = True
+    else:
+        datacheck = False
+
+
+targeted = ", ".join(targety)
+
+if datacheck is True:
+    print("""{0} added to https://twitter.com{1} which now has {2} members.
+    """.format(targeted, data['uri'], data['member_count']))
+else:
+    print("No users added to any list.")
