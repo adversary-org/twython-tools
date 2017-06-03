b08e3f1659f3763aa89d4c46ec5621a8c6fb37b3
Ben McGinnes <ben@adversary.org>
2017-06-03 20:05:49 +1000

    mylists-member-remove
    * Script to remove one user from multiple lists of one's own.

---
+++ ttscripts/mylists-member-remove.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+from __future__ import division
+
+##
+# Copyright (C) Ben McGinnes, 2013-2017
+# ben@adversary.org
+# OpenPGP/GPG key:  0x321E4E2373590E5D
+#
+# Version:  0.0.1
+#
+# BTC:  1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU
+# License:  BSD
+#
+#
+# Requirements:
+#
+# * Python 3.4 or later.
+#
+# Options and notes:
+#
+# Usage:
+#
+# __name__ <user> list1 list2 list3 etc.
+#
+##
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2017"
+__license__ = "BSD, Apache"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
+import math
+import sys
+import time
+from twython import Twython, TwythonError
+from config import *
+
+twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+
+"""
+Add a user to one or more lists.
+"""
+
+cred = twitter.verify_credentials()
+l = len(sys.argv)
+
+if l == 1:
+    username = input("Enter the ID or username to add to lists: ")
+    targetx = input("Enter lists/slugs (separate by spaces): ")
+elif l == 2:
+    username = sys.argv[1]
+    targetx = input("Enter lists/slugs (separate by spaces): ")
+elif l >= 3:
+    username = sys.argv[1]
+    targetx = " ".join(sys.argv[2:l])
+
+try:
+    userid = int(username)
+except:
+    userid = str(username)
+
+
+owner = cred["screen_name"]
+targetz = targetx.split()
+targety = []
+lt = len(targetz)
+
+for i in range(lt):
+    if targetz[i].isnumeric is True:
+        target1 = int(targetz[i])
+        target2 = str(targetz[i])
+    else:
+        target1 = str(targetz[i])
+        target2 = None
+
+    if target2 is None:
+        target = target1
+    else:
+        try:
+            t1a = twitter.show_user(user_id=target1)
+            target = t1a["screen_name"]
+        except:
+            target = target2
+
+    targety.append(target)
+
+dcl = []
+tl = len(targety)
+
+for i in range(tl):
+    try:
+        if tl < 15:
+            data = twitter.delete_list_member(slug=targety[i],
+                                           owner_screen_name=owner,
+                                           screen_name=userid)
+        else:
+            data = twitter.delete_list_member(slug=targety[i],
+                                           owner_screen_name=owner,
+                                           screen_name=userid)
+            time.sleep(62)
+    except TwythonError as e:
+        print(e)
+        data = None
+    if data is not None:
+        dcl.append(data)
+    else:
+        pass
+
+
+if len(dcl) > 0:
+    for i in range(len(dcl)):
+        print("""{0} removed from https://twitter.com{1} which now has {2} members.
+        """.format(userid, dcl[i]['uri'], dcl[i]['member_count']))
+else:
+    print("No users removed from any lists.")
