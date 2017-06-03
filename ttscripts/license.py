64e4b5bc93bb13fb3a1a8f2eabc2aaa75ac64069
Ben McGinnes <ben@adversary.org>
2017-06-03 23:26:16 +1000

    New license text.
    * Scripts to view license information.

---
+++ ttscripts/license.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+import datetime
+year = datetime.date.today().year
+
+__author__ = "Ben McGinnes <ben@adversary.org>"
+__copyright__ = "Copyright © Benjamin D. McGinnes, 2013-2017"
+__copyrighta__ = "Copyright (C) Benjamin D. McGinnes, 2013-2017"
+__copyright2__ = "Copyright © Benjamin D. McGinnes, 2013-{0}".format(year)
+__copyright2a__ = "Copyright (C) Benjamin D. McGinnes, 2013-{0}".format(year)
+__license__ = "Apache 2.0"
+__license_text__ = "Licenses/Apache.md"
+__version__ = "0.0.1"
+__bitcoin__ = "1KvKMVnyYgLxU1HnLQmbWaMpDx3Dz15DVU"
+
+boiler_generic = """
+    Copyright 2013-2017 Benjamin D. McGinnes
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+boiler_static_text = """
+    Copyright (C) Benjamin D. McGinnes, 2013-2017
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+boiler_static_utf8 = """
+    Copyright © Benjamin D. McGinnes, 2013-2017
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+"""
+
+boiler_dynamic_text = """
+    Copyright (C) Benjamin D. McGinnes, 2013-{0}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+""".format(year)
+
+boiler_dynamic_utf8 = """
+    Copyright © Benjamin D. McGinnes, 2013-{0}
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+""".format(year)
+
+boiler = boiler_static_utf8
+boiler2 = boiler_dynamic_utf8
